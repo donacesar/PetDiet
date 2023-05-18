e5b50342d38bda7126796414deaa79e899820855
donacesar <donacesar@gmail.com>
2023-05-18 16:29:42 +0300

    add BREEZE

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use App\Http\Controllers\Admin\IndexController;
 use App\Http\Controllers\Admin\TestController;
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin' , 'prefix'=>'admin'], function() {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
     Route::get('/', IndexController::class)->name('admin.index');
     Route::get('/test', TestController::class)->name('admin.test');
 
@@ -34,3 +35,15 @@ Route::get('/full-form', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
